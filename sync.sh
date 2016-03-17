fda28a3331ed94858347247c766e688a76db589c
dipack95 <dipack95@gmail.com>
2016-03-17 11:37:38 +0530

    Synced on date --iso-8601=seconds

--- sync.sh
+++ sync.sh
@@ -3,5 +3,5 @@ cp -Rv ~/.zshrc ~/Dotfiles/.zshrc
 cp -Rv ~/.bashrc ~/Dotfiles/.bashrc
 cp -Rv ~/.vimrc ~/Dotfiles/.vimrc
 git add .
-git commit -am 'Synced on $dateCmd'
+git commit -am "Synced on $dateCmd"
 git push -u origin master
