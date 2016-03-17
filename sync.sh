0848bce383e23e0fbed8dd523bfa8666024dddac
dipack95 <dipack95@gmail.com>
2016-03-17 15:15:45 +0530

    Synced on 2016-03-17T15:15:45+0530

--- sync.sh
+++ sync.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+
 dateCmd=$(date --iso-8601=seconds)
 declare -a filenames=("/home/dipack/.zshrc" "/home/dipack/.bashrc" "/home/dipack/.vimrc")
 for addFile in "${filenames[@]}"
 do 
     echo "Adding ${addFile} to git repo"
-    currFileName=${addFile//\/home\/dipack\/} 
     cp -Rv $addFile /home/dipack/Dotfiles
     #echo "$currFileName"
+    currFileName=${addFile//\/home\/dipack\/} 
     git add "$currFileName" 
 done
 git commit -am "Synced on $dateCmd"
 git push -u origin master
+echo "Finished syncing with remote repository"
