fc06f8d5d5ff0d541b5a11f08b4d2af9c61b7eb7
Dipack P Panjabi <dipack95@gmail.com>
2016-06-19 23:30:23 +0530

    'Sun Jun 19 23:30:23 2016'

--- zsh_conf/env.zsh
+++ zsh_conf/env.zsh
@@ -7,4 +7,6 @@ export ZSH_CONF_HOME=$HOME/zsh_conf
 export EDITOR="vim"
 export PATH="$HOME/anaconda3/bin:$HOME/bin:$PATH"
 export PROSODIZER_SOUND_FILES="$HOME/College/Fourth_Year/Final_Year_Project/sounds"
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket" 
+export SSH_ASKPASS="/usr/bin/ksshaskpass"
 # End of Environment Variables
