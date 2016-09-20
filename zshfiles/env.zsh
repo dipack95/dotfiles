2e34b9ad0258c68ba8086aa65f03b15b823edb47
Dipack P Panjabi <dipack95@gmail.com>
2016-09-20 21:06:23 +0530

    'Tue Sep 20 21:06:23 2016'

---
+++ zshfiles/env.zsh
@@ -0,0 +1,13 @@
+
+# Environment Variables
+
+# Path to your oh-my-zsh installation.
+export ZSH=$HOME/.oh-my-zsh
+export ZSH_CONF_HOME=$HOME/zshfiles
+export EDITOR="vim"
+export PATH="$HOME/anaconda3/bin:$HOME/bin:$PATH"
+export PROSODIZER_SOUND_FILES="$HOME/College/Fourth_Year/Final_Year_Project/sounds"
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket" 
+export SSH_ASKPASS="/usr/bin/ksshaskpass"
+export LESS='-RMi'
+# End of Environment Variables
---
+++ zshfiles/functions.zsh
@@ -0,0 +1,93 @@
+# make a backup of a file
+# https://github.com/grml/grml-etc-core/blob/master/etc/zsh/zshrc
+bk() {
+	cp -a "$1" "${1}_$(date --iso-8601=seconds)"
+}
+
+# print a separator banner, as wide as the terminal
+function hr {
+	print ${(l:COLUMNS::=:)}
+}
+
+# get public ip
+function myip {
+	local api
+	case "$1" in
+		"-4")
+			api="http://v4.ipv6-test.com/api/myip.php"
+			;;
+		"-6")
+			api="http://v6.ipv6-test.com/api/myip.php"
+			;;
+		*)
+			api="http://ipv6-test.com/api/myip.php"
+			;;
+	esac
+	curl -s "$api"
+	echo # Newline.
+}
+
+# Create short urls via http://goo.gl using curl(1).
+# Contributed back to grml zshrc
+# API reference: https://code.google.com/apis/urlshortener/
+function zurl {
+	if [[ -z $1 ]]; then
+		print "USAGE: $0 <URL>"
+		return 1
+	fi
+
+	local url=$1
+	local api="https://www.googleapis.com/urlshortener/v1/url"
+	local data
+
+	# Prepend "http://" to given URL where necessary for later output.
+	if [[ $url != http(s|)://* ]]; then
+		url="http://$url"
+	fi
+	local json="{\"longUrl\": \"$url\"}"
+
+	data=$(curl --silent -H "Content-Type: application/json" -d $json $api)
+	# Match against a regex and print it
+	if [[ $data =~ '"id": "(http://goo.gl/[[:alnum:]]+)"' ]]; then
+		print $match
+	fi
+}
+
+dot_progress() {
+    # Fancy progress function from Landley's Aboriginal Linux.
+    # Useful for long rm, tar and such.
+    # Usage:
+    #     rm -rfv /foo | dot_progress
+    local i='0'
+    local line=''z
+ 
+    while read line; do
+        i="$((i+1))"
+        if [ "${i}" = '25' ]; then
+            printf '.'
+            i='0'
+        fi
+    done
+    printf '\n'
+}
+ 
+function extract() {
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)   tar xvjf $1 | dot_progress     ;;
+            *.tar.gz)    tar xvzf $1 | dot_progress     ;;
+            *.bz2)       bunzip2 $1 | dot_progress      ;;
+            *.rar)       unrar x $1 | dot_progress      ;;
+            *.gz)        gunzip $1 | dot_progress       ;;
+            *.tar)       tar xvf $1 | dot_progress      ;;
+            *.tbz2)      tar xvjf $1 | dot_progress    ;;
+            *.tgz)       tar xvzf $1 | dot_progress     ;;
+            *.zip)       unzip $1 | dot_progress        ;;
+            *.Z)         uncompress $1 | dot_progress   ;;
+            *.7z)        7z x $1 | dot_progress         ;;
+            *)           echo "'$1' cannot be extracted via >extract<" ;;
+        esac
+    else
+        echo "'$1' is not a valid file!"
+    fi
+}
