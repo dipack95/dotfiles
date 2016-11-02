6bd416c253f1009f949f2264e05af79afd2ec08e
Dipack P Panjabi <dipack95@gmail.com>
2016-11-02 18:14:24 +0530

    'Wed Nov  2 18:14:24 2016'

--- zshfiles/aliases.zsh
+++ zshfiles/aliases.zsh
@@ -3,4 +3,4 @@ alias reload=". ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
 alias ppu="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install -U"
 alias pp3u="pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip3 install -U"
 alias rmrecent='rm $HOME/.local/share/recently-used.xbel; touch $HOME/.local/share/recently-used.xbel'
-# End of Aliases	
+# End of Aliases
