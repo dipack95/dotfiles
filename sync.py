5501da3d17fdbac75a73acb2090087bf27ce15c9
Dipack P Panjabi <dipack95@gmail.com>
2017-01-14 01:11:16 +0530

    Modded sync file

--- sync.py
+++ sync.py
@@ -3,14 +3,13 @@ import sys
 import os
 import time
 import shutil
-import git
 from subprocess import call
 # Check if running with python 3 or newer
 if sys.version_info[0] != 3:
         print("This script requires Python version 3.x")
         sys.exit(1)
 
-def addToRepo(directory, filenames, repo):
+def addToRepo(directory, filenames):
     currDir = os.getcwd()
     os.chdir(directory)
     for tempFile in filenames:
@@ -21,7 +20,7 @@ def addToRepo(directory, filenames, repo):
             call(["cp", "-R", tempFile, directory + "/" + toBeAdded])
         else:
             call(["cp", "-R", tempFile, directory])
-        repo.git.add(toBeAdded)
+        call(["git", "add", toBeAdded])
     return 1 
 def cleanUp(directory, filenames):
     currDir = os.getcwd()
@@ -36,7 +35,6 @@ def cleanUp(directory, filenames):
     return 1
 
 def main():
-    dotfilesRepo = git.Repo(os.getcwd())
     filenames = ["$HOME/.zshrc",
             "$HOME/zshfiles",
             "$HOME/.vimrc",
@@ -50,17 +48,15 @@ def main():
     else:
         print("Failed to clean up current directory.")
         return 0
-    if (addToRepo(os.getcwd(), filenames, dotfilesRepo)):
+    if (addToRepo(os.getcwd(), filenames)):
         print("Copied from source location to current directory, and added to git repo.")
     else:
         print("Failed to copy from source location, and add to git repo.")
         return 0
-    # call(["git", "commit", "-am", "\'" + time.strftime("%c") + "\'"])
-    # call(["git", "push", "-u", "--quiet", "origin", "master"])
-    if(dotfilesRepo.is_dirty()):
+    print(call(["git", "status", "--porcelain"]))
+    if(call(["git", "status", "--porcelain"])):
         call(["git", "commit", "-am", "\'" + time.strftime("%c") + "\'"])
-        #dotfilesRepo.git.commit(m = time.strftime("%c"))
-        dotfilesRepo.git.push()
+        call(["git", "push", "-u", "--quiet", "origin", "master"])
         print("Synced with remote repository")
     else:
         print("Nothing to commit!")
