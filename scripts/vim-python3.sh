d479330a500e79e0cfde468241108d1ce24353b5
Dipack P Panjabi <dipack95@gmail.com>
2017-03-06 00:40:08 +0530

    'Mon Mar  6 00:40:08 2017'

--- scripts/vim-python3.sh
+++ scripts/vim-python3.sh
@@ -2,12 +2,11 @@
 echo "Getting prerequisites for this build..."
 
 sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
-        libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-	    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-	        python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
+    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
 
-
-echo "Starting VIM Python 3 build.."
+echo "Starting VIM Python 3 build"
 
 if [ ! -d ./vim-build/ ]; then
     mkdir -p ./vim-build
@@ -20,6 +19,7 @@ git clone https://github.com/vim/vim.git vim-git
 cd ./vim-git
 
 VIM_PYTHON3_SETUP="$(python3-config --configdir)"
+VIM_VERSION="$(head -1 README.txt | sed -r 's/[^0-9]//g')"
 echo "Python 3 Config directory is: " $VIM_PYTHON3_SETUP
 
 ./configure --with-features=huge --enable-multibyte --enable-rubyinterp --enable-python3interp --with-python3-config-dir=$VIM_PYTHON3_SETUP --enable-perlinterp --enable-luainterp --enable-gui=gtk2 --enable-cscope --prefix=/usr
@@ -27,7 +27,7 @@ echo "Python 3 Config directory is: " $VIM_PYTHON3_SETUP
 make VIMRUNTIMEDIR=/usr/share/vim/vim80
 
 echo "Using checkinstall to build and install custom VIM"
-sudo checkinstall
+sudo checkinstall --pkgname=vim-py3 --pkgversion=$VIM_VERSION-git --provides=vim
 
 unset $VIM_PYTHON3_SETUP
 
