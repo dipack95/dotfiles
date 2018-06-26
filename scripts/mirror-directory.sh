8086b8a85b23d5ec1d7912c48cc09abf752967e0
Dipack P Panjabi <dipack95@gmail.com>
2018-06-26 22:28:16 +0530

    'Tue Jun 26 22:28:16 2018'

--- scripts/mirror-directory.sh
+++ scripts/mirror-directory.sh
@@ -9,6 +9,13 @@
 source="$1";
 target="$2";
 
+if rclone -v lsf remote:Dropbox_Mirror; then
+    echo "RClone connection to Dropbox_Mirror is working as expected";
+else
+    echo "RClone connection to Dropbox_Mirror is not working!";
+    exit 255;
+fi
+
 if [ -z "$source" ] || [ -z "$target" ]; then
     echo "Source or target not set.";
     echo "Source: $source";
@@ -21,5 +28,14 @@ echo "Mirroring $source to $target";
 echo
 
 while inotifywait --recursive --event modify,create,delete $source; do
-    rsync --archive --update --delete --one-file-system --info=progress2 --exclude=".*" --exclude="Skydiving" --exclude=".dropbox" --exclude="~*" $source $target;
+    # --archive         : Basically copies the files and subdirs in archive mode, i.e. preserving perms, etc.
+    # --one-file-system : Prevent moving across different FS types, i.e. if an NTFS partition is mounted at the $source, and the current FS is Ext4, do not look at the NTFS partition
+    # --info=progress2  : Provides a progress log to show you what is happening
+    # The trailing slash at the end of `$source` is important, as it prevents a folder with the $source's name from being created on the $target
+    rsync --archive --update --delete --one-file-system --info=progress2 --exclude=".*" --exclude="Skydiving" --exclude=".dropbox" --exclude="~*" $source/ $target;
+    if rclone sync --stats=10s --stats-log-level=NOTICE $target remote:Dropbox_Mirror; then
+        echo
+        echo "Completed RClone sync to remote:Dropbox_Mirror";
+        echo
+    fi
 done
